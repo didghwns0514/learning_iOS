a161f86cdf8a7a29069bb0f5cd69edc7480a6423
HoJun Yang <didghwns0514@gmail.com>
2022-06-06 15:40:29 +0900

    doc(lecture_notes.md):Update Autolayout markdown

--- project_dir/UpDownGame/UpDownGame/ViewController.swift
+++ project_dir/UpDownGame/UpDownGame/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb"), for: .normal)
+        slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb"), for: .normal)
     }
     
     @IBAction
